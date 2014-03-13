d0f48ef0080bd5c087695c9bb0563f263a75629a
Reinier Ladan <reinierl@gmail.com>
2014-03-13 13:04:15 +0100

    Improved the hamburger navigation

--- app/javascript/navigation.js
+++ app/javascript/navigation.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
 	$('.open-navigation').click(function(){
-		$('nav').toggle();
-		$('body').toggleClass('navigation-is-open');
+
+		if($('body').hasClass('navigation-is-open')){
+			$('body').removeClass('navigation-is-open');
+		} else {
+			$('body').addClass('navigation-is-open');
+		}
 	});
 });
